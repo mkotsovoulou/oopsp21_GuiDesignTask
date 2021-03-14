ccbe85d709267a9d304ef9092bb2159b95d2372f
Maira Kotsovoulou <f-mkotsovoulou@acgadmin.edu>
2021-03-14 20:18:28 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class Controller  {
+
+}
